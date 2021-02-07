04e5889e93ed651d4fafcba0403206012cdd2745
Nick Rose <nihk@github.com>
2021-02-07 17:49:41 -0500

    :zap:

---
+++ app/src/main/java/nick/filefun/MainActivity.kt
@@ -0,0 +1,16 @@
+package nick.filefun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class MainViewModel : ViewModel() {
+
+}
